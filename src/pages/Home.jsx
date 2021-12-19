dda3684702964703158d2688f6e3ba911f4c2b60
sam002696 <sadmansakib221@gmail.com>
2021-12-19 20:53:31 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Announcement from '../components/Announcement'
+import Categories from '../components/Catergories'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import Newsletter from '../components/Newsletter'
+import Products from '../components/Products'
+import Slider from '../components/Slider'
+
+const Home = () => {
+    return (
+        <div>
+            <Announcement></Announcement>
+            <Navbar></Navbar>
+            <Slider></Slider>
+            <Categories></Categories>
+            <Products></Products>
+            <Newsletter></Newsletter>
+            <Footer></Footer>
+        </div>
+    )
+}
+
+export default Home
+
+
